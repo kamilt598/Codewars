4914c64e1dac3798b7fe9b036bdb0568cdee9403
kamilt598 <kamilt598@gmail.com>
2021-12-12 11:39:29 +0100

    Solved task MinMax

---
+++ src/main/java/MinMax.java
@@ -0,0 +1,15 @@
+public class MinMax {
+    public static int[] minMax(int[] arr) {
+        int min = arr[0];
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] < min) {
+                min = arr[i];
+            }
+            if(arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return new int[]{min, max};
+    }
+}
